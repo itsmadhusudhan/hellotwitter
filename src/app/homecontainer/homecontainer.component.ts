c985eecad8d8535b04d6b933bc52174733a28908
itsmadhusudhan <madhusudhan1863@gmail.com>
2018-04-15 11:02:11 +0530

    updated profile card with data from parent

--- src/app/homecontainer/homecontainer.component.ts
+++ src/app/homecontainer/homecontainer.component.ts
@@ -9,6 +9,18 @@ export class HomecontainerComponent implements OnInit {
 
   constructor() { }
 
+  userInfo:Object={
+    userFullname:"Madhusudhan R",
+    username:"itsmadhusudhan",
+    tweets:61,
+    following:51,
+    followers:21,
+    likes:102,
+    lists:0,
+    moments:0
+  }
+
+
   ngOnInit() {
   }
 
--- src/app/profilecard/profilecard.component.ts
+++ src/app/profilecard/profilecard.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-profilecard',
-  templateUrl: './profilecard.component.html',
-  styleUrls: ['./profilecard.component.css']
+  selector: "app-profilecard",
+  templateUrl: "./profilecard.component.html",
+  styleUrls: ["./profilecard.component.css"]
 })
 export class ProfilecardComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Input() userInfo;
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
